f1c76ec81a2eb1aa90aec2ee8f5a95f1169b2648
monank sojitra <monaksojitra4444@gmail.com>
2024-02-03 19:23:31 +0530

    fix slider problem

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -3,49 +3,28 @@ import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
-import {
-  FaRegArrowAltCircleLeft,
-  FaRegArrowAltCircleRight,
-} from "react-icons/fa";
 import TitelBox from "./TitelBox";
 import { ListofProject } from "@/data/ProjectData";
 
 interface ProjectListProps {}
 
 const ProjectList: React.FC<ProjectListProps> = () => {
-  const sliderRef = useRef<SwiperRef | null>(null);
-
-  const handlePrev = useCallback(() => {
-    if (sliderRef.current && sliderRef.current.swiper) {
-      sliderRef.current.swiper.slidePrev();
-    }
-  }, []);
-
-  const handleNext = useCallback(() => {
-    if (sliderRef.current && sliderRef.current.swiper) {
-      sliderRef.current.swiper.slideNext();
-    }
-  }, []);
+  // const swiperRef = useRef<SwiperRef | null>(null);
 
   return (
     <div className="my-3 mt-10 " id="project">
       <TitelBox titel="Project's" css="my-3 text-lg md:text-3xl" />
-      <FaRegArrowAltCircleLeft
+      {/* <FaRegArrowAltCircleLeft
         size={30}
-        onClick={handlePrev}
-        className="absolute left-2 sm:left-6 translate-y-32 z-20"
+        onClick={() => swiperRef.current?.slidePrev()}
+        className="absolute left-2 sm:left-6 translate-y-32 z-20 cursor-pointer"
       />
       <FaRegArrowAltCircleRight
         size={30}
-        onClick={handleNext}
-        className="absolute right-2 sm:right-6 translate-y-32  z-20"
-      />
+        onClick={() => swiperRef.current?.slideNext()}
+        className="absolute right-2 sm:right-6 translate-y-32  z-20 cursor-pointer"
+      /> */}
       <Swiper
-        ref={(instance) => {
-          if (instance) {
-            sliderRef.current = instance;
-          }
-        }}
         breakpoints={{
           320: {
             slidesPerView: 1,
@@ -61,17 +40,18 @@ const ProjectList: React.FC<ProjectListProps> = () => {
           },
         }}
         slidesPerView={1}
+        navigation={true}
         spaceBetween={30}
         centeredSlides={true}
         modules={[Pagination]}
-        className="mySwiper scroll-smooth"
+        className="mySwiper scroll-smooth  overflow-x-scroll no-scrollbar "
       >
         {ListofProject.map((item, index: number) => (
           <SwiperSlide key={index}>
             <iframe
-              className="no-scrollbar"
+              className="no-scrollbar "
               src={item.linkedinEmbed}
-              height="300"
+              height="400"
               width={window.innerWidth > 450 ? "400" : "270"}
             ></iframe>
           </SwiperSlide>
